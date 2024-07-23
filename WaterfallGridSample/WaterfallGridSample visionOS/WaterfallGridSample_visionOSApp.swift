ac68f5a7d1b74f31eafe0fe6f7b7a245a8dc896f
Paolo Leonardi <pao.leonardi@gmail.com>
2024-07-23 01:14:04 +0100

    Add visionOS target to WaterfallGridSample

---
+++ WaterfallGridSample/WaterfallGridSample visionOS/WaterfallGridSample_visionOSApp.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2024 Paolo Leonardi. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WaterfallGridSample_visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
